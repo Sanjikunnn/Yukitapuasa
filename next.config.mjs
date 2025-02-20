96a05c6837086503a0047232dc93e48768b02be6
Sanjikunnn <fmiqbal66@gmail.com>
2025-02-20 22:34:59 +0700

    api geo nya ga run wkwk

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 // next.config.mjs
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ['www.masakapahariini.com'],
   },
 };
 
 export default nextConfig;
-
-
